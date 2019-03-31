2ca53470e443f825af693cb75045e20a6b7dfb8c
Daniel Hernandez <tamdannyhdz@gmail.com>
2019-03-30 23:14:18 -0600

    Add preliminary to buttons when touched

--- calculadora-bases/calculadora-bases/CalculatorViewController.swift
+++ calculadora-bases/calculadora-bases/CalculatorViewController.swift
@@ -6,6 +6,18 @@ class CalculatorViewController: UIViewController {
 	@IBOutlet weak var lbEquation: UILabel!
 	
 	let calculator = Calculator()
+	let buttonColors = [
+		UIColor(hex: 0xffffff),
+		UIColor(hex: 0xfc3951),
+		UIColor(hex: 0xdd86fc),
+		UIColor(hex: 0x555555)
+	]
+	let activeColors = [
+		UIColor(hex: 0xe4e4e4),
+		UIColor(hex: 0xce2f42),
+		UIColor(hex: 0xca7be6),
+		UIColor(hex: 0x3d3d3d)
+	]
 	var result = Number(base: 10, integralPart: "0", fractionalPart: nil)
 	var activeOperation = 0
 	var hasTyped = false
@@ -21,8 +33,18 @@ class CalculatorViewController: UIViewController {
 	// Activates when a button is held down
 	@IBAction func buttonHold(_ sender: UIButton) {
 		// Alter background color and border
-		sender.layer.borderColor = UIColor(rgb: 0x191919).cgColor
+		sender.layer.borderColor = UIColor(hex: 0x191919).cgColor
 		sender.layer.borderWidth = 3
+		
+		if sender.tag <= 10 {		// 0..9 & . buttons
+			sender.backgroundColor = activeColors[0]
+		} else if sender.tag == 11 {		// Delete button
+			sender.backgroundColor = activeColors[1]
+		} else if sender.tag <= 14 {		// =, +, & - buttons
+			sender.backgroundColor = activeColors[2]
+		} else if sender.tag <= 18 {		// Functions buttons
+			sender.backgroundColor = activeColors[3]
+		}
 	}
 	
 	// Activates when a button is pressed
@@ -30,8 +52,6 @@ class CalculatorViewController: UIViewController {
 		sender.layer.borderWidth = 0			// Reset border
 		
 		switch sender.tag {
-			// Numpad button (0-9, A-F, .)
-			// Note: Case 10 (.) is omitted because decimals have not been implemented yet
 		case 0,
 				 1,
 				 2,
@@ -41,30 +61,43 @@ class CalculatorViewController: UIViewController {
 				 6,
 				 7,
 				 8,
-				 9:
+				 9:			// Numpad button (0-9, A-F, .) Note: Case 10 (.) is omitted because decimals have not been implemented yet
 			typeDigit(digit: Character(String(sender.tag)))
+			sender.backgroundColor = buttonColors[0]
 			
-			// Delete button
-		case 11:
+		case 11:		// Delete button
 			deleteDigit()
+			sender.backgroundColor = buttonColors[1]
 			
-			// Equal button
-		case 12:
+		case 12:		// Equal button
 			if activeOperation != 0 || hasTyped {
 				operation(nextOp: 0)
 			}
+			sender.backgroundColor = buttonColors[2]
 			
-			// Add button
-		case 13:
+		case 13:		// Add button
 			if activeOperation != 1 || hasTyped {
 				operation(nextOp: 1)
 			}
+			sender.backgroundColor = buttonColors[2]
 			
-			// Subtract button
-		case 14:
+		case 14:		// Subtract button
 			if activeOperation != 2 || hasTyped {
 				operation(nextOp: 2)
 			}
+			sender.backgroundColor = buttonColors[2]
+			
+		case 15:		// '2nd' button
+			sender.backgroundColor = buttonColors[3]
+			
+		case 16:		// Change polarity button
+			sender.backgroundColor = buttonColors[3]
+			
+		case 17:		// Change base button
+			sender.backgroundColor = buttonColors[3]
+			
+		case 18:		// Settings button
+			sender.backgroundColor = buttonColors[3]
 			
 		default:
 			print("ERROR: Invalid button?")
@@ -142,11 +175,11 @@ extension UIColor {
 		self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
 	}
 	
-	convenience init(rgb: Int) {
+	convenience init(hex: Int) {
 		self.init(
-			red: (rgb >> 16) & 0xFF,
-			green: (rgb >> 8) & 0xFF,
-			blue: rgb & 0xFF
+			red: (hex >> 16) & 0xFF,
+			green: (hex >> 8) & 0xFF,
+			blue: hex & 0xFF
 		)
 	}
 }
